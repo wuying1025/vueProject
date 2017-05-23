94fb3f961dbce357b393360c37bf3577b6cbadcc
wuying <wuy_123@126.com>
2017-05-23 16:50:08 +0800

    vuedemo

---
+++ src/components/photo.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class='photo'>
+      <common-top></common-top>
+      <router-view></router-view>
+      <common-bottom></common-bottom>
+   </div>
+</template>
+
+<script>
+import CommonTop from './common/CommonTop.vue'
+import CommonBottom from './common/CommonBottom.vue'
+export default {
+    components:{
+      CommonTop,
+      CommonBottom
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
